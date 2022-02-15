f9354f39888e69cdd4716dd07f9d2eae5276b287
izoj2013 <mivieux@yahoo.com>
2022-02-16 00:06:33 +0200

    Commit_4_deployment_16.02.2022

--- acrsite/settings.py
+++ acrsite/settings.py
@@ -143,13 +143,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "/static",
-    'var/www/static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     'var/www/static',
+# ]
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 # Media files (images, videos)
 
 MEDIA_ROOT = BASE_DIR / 'uploads'
